495137c6555d265eafaa7f14c9be870952df6d5f
Russell Gordon <github@russellgordon.ca>
2022-05-09 05:39:05 -0400

    Initial Commit

---
+++ LocalNotificationsExample/LocalNotificationsExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  LocalNotificationsExampleApp.swift
+//  LocalNotificationsExample
+//
+//  Created by Russell Gordon on 2022-05-09.
+//
+
+import SwiftUI
+
+@main
+struct LocalNotificationsExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
